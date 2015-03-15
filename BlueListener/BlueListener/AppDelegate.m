390b0cbc71cb322c94f383133d79749c16d41d21
melissa boring <melbrng@gmail.com>
2015-03-15 14:44:53 -0700

    initial commit

---
+++ BlueListener/BlueListener/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  BlueListener
+//
+//  Created by Melissa Boring on 3/15/15.
+//  Copyright (c) 2015 Melissa Boring. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    return YES;
+}
+
+
+
+@end
---
+++ BlueListener/BlueListener/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  BlueListener
+//
+//  Created by Melissa Boring on 3/15/15.
+//  Copyright (c) 2015 Melissa Boring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface ViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate, UITextViewDelegate>
+
+- (IBAction)startScan:(UIButton *)sender;
+- (IBAction)clearOutput:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UITextView *scanTextView;
+
+
+@end
+
